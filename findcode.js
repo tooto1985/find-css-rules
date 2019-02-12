fc69d0efc1bf109edcdeeb04a3db703bff2b7f47
Tommy <tooto1985@gmail.com>
2019-02-12 16:05:55 +0800

    add

---
+++ findcode.js
@@ -0,0 +1,56 @@
+var fs = require('fs')
+var path = require('path')
+
+// ===========
+
+var findPath = 'D:\\Tommy\\SourceCode\\OBWeb\\OBWeb\\css'
+var findType = '.css'
+var findRegExp = /.+(?=\{)/g
+var ary = []
+var rules = {}
+
+// ===========
+
+var walkSync = function (dir, list, type) {
+  var files = fs.readdirSync(dir)
+  list = list || []
+  files.forEach(function (file) {
+    if (fs.statSync(dir + '\\' + file).isDirectory()) {
+      walkSync(dir + '\\' + file, list, type)
+    } else {
+      if (path.extname(file) === type) {
+        list.push(dir + '\\' + file)
+      }
+    }
+  })
+  return list
+}
+
+var recursion = function (ary, callback) {
+  ary.forEach(callback)
+}
+
+var searchCode = function (file, regexp, rules) {
+  var data = fs.readFileSync(file).toString()
+  data.match(regexp).map(function (item) {
+    return item.split(' ')[0].split(':')[0]
+  }).filter(function (item) {
+    return /^(\.|#)/.test(item)
+  }).forEach(function (item) {
+    rules[item.substr(1)] = 1
+  })
+}
+
+// ===========
+
+walkSync(findPath, ary, findType)
+
+recursion(ary, function (item) {
+  searchCode(item, findRegExp, rules)
+})
+
+console.log(Object.keys(rules))
+
+fs.writeFileSync('./output.txt', Object.keys(rules).join('\n'))
+
+// searchCode('D:\\Tommy\\SourceCode\\OBWeb\\OBWeb\\css\\login.css', findRegExp)
